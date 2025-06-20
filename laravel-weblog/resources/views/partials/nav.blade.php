b3f3e7599ee968f026cc1b79919feb09a3be2c6d
scriptklaas <klaasettema34@gmail.com>
2025-06-20 13:48:14 +0200

    final commit - removed duplicate laravel-weblog folder inside this one

--- laravel-weblog/resources/views/partials/nav.blade.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="{{ route('posts.login') }}">Login</a></li>
-        <li><a href="{{ route('posts.logout') }}">Logout</a></li>
-        <li><a href="{{ route('posts.index') }}">Posts</a></li>
-        <li><a href="{{ route('posts.create') }}">Create post</a></li>
-        <li><a href="{{ route('categories') }}">Categories</a></li>
-    </ul>
-</nav>
